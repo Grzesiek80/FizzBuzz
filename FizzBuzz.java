469639f8c96f1966158b26368b561808baa09d39
Grzegorz Gil <grzechgil@poczta.onet.pl>
2018-06-15 19:16:55 +0200

    commit

---
+++ FizzBuzz.java
@@ -0,0 +1,23 @@
+package Enum;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
+                System.out.println("FizzBuzz " + i);
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Fizz " + i);
+
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Buzz " + i);
+
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
